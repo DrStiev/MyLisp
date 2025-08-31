7af4d421cb47c8b59f4ee03f2c2462ba3b77fbe0
DrStiev <mstievano9@gmail.com>
2025-08-31 10:35:39 +0200

    evaluation

--- src/lispy.c
+++ src/lispy.c
@@ -3,6 +3,53 @@
 
 #include "parser/mpc.h"
 
+// use operator string to see which operation to perform
+long eval_op(long x, char *op, long y)
+{
+    if (strcmp(op, "+") == 0)
+    {
+        return x + y;
+    }
+    if (strcmp(op, "-") == 0)
+    {
+        return x - y;
+    }
+    if (strcmp(op, "*") == 0)
+    {
+        return x * y;
+    }
+    if (strcmp(op, "/") == 0)
+    {
+        return x / y;
+    }
+    return 0;
+}
+
+long eval(mpc_ast_t *t)
+{
+    // if tagged as number return it directly
+    if (strstr(t->tag, "number"))
+    {
+        return atoi(t->contents);
+    }
+
+    // the operator is always second child
+    char *op = t->children[1]->contents;
+
+    // store the third child in x
+    long x = eval(t->children[2]);
+
+    // iterate the remaining children and combining them
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr"))
+    {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 // if we are compiling on windows compile these functions
 #ifdef _WIN32
 #include <string.h>
@@ -61,7 +108,7 @@ int main(int argc, char **argv)
               Number, Operator, Expr, Lispy);
 
     // print Version and Exit information
-    puts("C-Lisp \"Lispy\" Version 0.0.0.0.2");
+    puts("C-Lisp \"Lispy\" Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     while (1)
@@ -74,8 +121,9 @@ int main(int argc, char **argv)
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r))
         {
-            // on success print the AST
-            mpc_ast_print(r.output);
+            // begin evaluation
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         }
         else
